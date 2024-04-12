735d5a848c8f68c85bc1dd86884cd0643fa67bfb
Cindy Ding <cindyding@s-169-232-126-38.resnet.ucla.edu>
2024-04-11 18:34:51 -0700

    Initialized the git repo and included class code
    I just added the course, so I initialized the git repository and added what we did in class today (second class) to a repository.

---
+++ Assets/MafiaShoot.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MafiaShoot : MonoBehaviour
+{
+    [SearializeField]
+    GameObject bullet;
+
+    int counter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter++;
+        if (counter % 60 == 0)
+        {
+            Instantiate(bullet, transform.position, Quaternion.identity);
+        }
+        
+    }
+}
